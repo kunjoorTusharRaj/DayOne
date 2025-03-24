7c7e8a1e136c51b2a8cc871185887051fb940cf2
tushar-19459 <19459tusharraj@gmail.com>
2025-03-24 17:03:47 +0530

    i have added the exception handling block

---
+++ tushar.java
@@ -0,0 +1,9 @@
+public class tushar {
+    public static void main(String[] args) {
+        try {
+            System.out.println("this is tushar.java");
+        } catch (Exception e) {
+            System.out.println("Excetpion "+e);
+        }
+    }
+}
