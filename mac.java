36d5dcca965b30ec69901aa8a9104b43a04a442c
tushar-19459 <19459tusharraj@gmail.com>
2025-03-24 17:44:11 +0530

    some new updateds

---
+++ mac.java
@@ -0,0 +1,9 @@
+public class mac {
+    public static void main(String[] args) {
+        try {
+            System.out.println("this is the mac.");
+        } catch (Exception e) {
+            System.err.println("Error message "+e);
+        }
+    }
+}
--- one.java
+++ one.java
@@ -1,9 +1,10 @@
 public class one {
     public static void main(String[] args) {
-        System.out.println("hellow world");
-    
-    
-    
-    
+        try {
+            System.out.println("this is one.java");
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("error message "+e.getMessage());
+        }        
     }
 }
