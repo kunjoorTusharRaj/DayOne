57c0aecbd99d6cf92e8cf9c70e7a8e813ecc0f40
tushar-19459 <19459tusharraj@gmail.com>
2025-03-24 17:53:34 +0530

    removed the comment

--- one.java
+++ one.java
@@ -3,7 +3,6 @@ public class one {
         try {
             System.out.println("this is one.java");
         } catch (Exception e) {
-            // TODO: handle exception
             System.out.println("error message "+e.getMessage());
         }        
     }
